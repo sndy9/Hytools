38ba9886913c2bd63a5b85ceae4ecbd20712f517
sndy9 <70355219+sndy9@users.noreply.github.com>
2020-08-28 13:27:18 -0700

    Add files via upload

--- Hytools.java
+++ Hytools.java
@@ -1,14 +1,17 @@
 package com.sndy.Hytools;
 // Created by sndy on 7/11/2020.
 import com.sndy.Hytools.CommandManager.HytoolsCommands;
+import com.sndy.Hytools.DataManager.Profile;
 import com.sndy.Hytools.GUIManager.GUIMain;
+import com.sndy.Hytools.StatManager.PlayerAction;
 import net.minecraftforge.client.ClientCommandHandler;
-import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.io.File;
 
 @Mod(modid = Hytools.MODID, version = Hytools.VERSION)
 public class Hytools { //main controller class, searches for messages
@@ -19,14 +22,27 @@ public class Hytools { //main controller class, searches for messages
     public static boolean modEnabled = false;
 
     @EventHandler
-    public void init(FMLPostInitializationEvent event) { //register events here
+    public void init(FMLInitializationEvent event) { //register events here
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(new GUIMain());
         ClientCommandHandler.instance.registerCommand(new HytoolsCommands());
     }
 
-    @SubscribeEvent
-    public static void onChat(ClientChatReceivedEvent event){
-
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+        PlayerAction.profile = "default";
+        File tmpDir = new File("Hytools");
+        if (!tmpDir.exists()){
+            //create base directories (only activates on first use)
+            File directory1 = new File("Hytools");
+            if (directory1.mkdir()){
+                System.out.println("[HYTOOLS]: Hytools directory successfully created!");
+                File directory2 = new File("Hytools/default");
+                if (directory2.mkdir()){
+                    System.out.println("[HYTOOLS]: default profile directory successfully created!");
+                    Profile.saveStats(PlayerAction.profile);
+                }
+            }
+        }
     }
 }
