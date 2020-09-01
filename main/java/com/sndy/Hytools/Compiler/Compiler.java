4c7ea7707fc1551fbd5232c869074c587d492111
tigerrex9 <professorracecar@gmail.com>
2020-09-01 12:08:11 -0700

    added splitter to Compiler

--- main/java/com/sndy/Hytools/Compiler/Compiler.java
+++ main/java/com/sndy/Hytools/Compiler/Compiler.java
@@ -1,5 +1,19 @@
 package com.sndy.Hytools.Compiler;
 
-public class Compiler {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+public class Compiler {
+    public static void Compile(String FilePath){
+        String Contents = "";
+        try{
+            Contents = Files.readString(Paths.get(FilePath));
+        }
+        catch (IOException exception){
+            exception.printStackTrace();
+        }
+
+        Contents.split(";");
+    }
 }
